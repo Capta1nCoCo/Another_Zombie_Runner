976f7dc659227f478e1ddc2fcfef997fb1572520
Capta1nCoCo <femabu28@gmail.com>
2020-07-20 11:18:52 +0300

    178. Implement A Muzzle Flash

--- Another_Zombie_Runner/Assets/Scripts/Weapon.cs
+++ Another_Zombie_Runner/Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 60f;
+    [SerializeField] ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -19,19 +20,30 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
             // TODO: add some hit effect for visual players
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if (target == null) return; 
+            if (target == null) return;
             target.TakeDamage(damage);
         }
         else
         {
             return;
         }
-        
     }
 }
