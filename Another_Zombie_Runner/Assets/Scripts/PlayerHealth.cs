1e88dd032d5a14f2e3ad54e6a8da9ac830d10050
Capta1nCoCo <femabu28@gmail.com>
2020-07-20 17:17:27 +0300

    185. Create Player Health Class

---
+++ Another_Zombie_Runner/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+        
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Debug.Log("You Died");
+        }
+    }
+    
+}
