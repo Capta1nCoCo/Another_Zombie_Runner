532bc858198906ced88ef6fa00e2c7580d296ed6
Capta1nCoCo <femabu28@gmail.com>
2020-07-20 10:30:59 +0300

    177. Enemy Health & Damage

--- Another_Zombie_Runner/Assets/Scripts/Weapon.cs
+++ Another_Zombie_Runner/Assets/Scripts/Weapon.cs
@@ -7,6 +7,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 60f;
 
     void Update()
     {
@@ -19,7 +20,18 @@ public class Weapon : MonoBehaviour
     private void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log(hit.transform.name);
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            // TODO: add some hit effect for visual players
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return; 
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+        
     }
 }
