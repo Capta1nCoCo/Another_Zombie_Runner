85f2f3c301e804e0b5127cdcc526423cff9620fb
Capta1nCoCo <femabu28@gmail.com>
2020-07-21 09:31:23 +0300

    187. Game Over User Interface

---
+++ Another_Zombie_Runner/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    public void ReStart()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
