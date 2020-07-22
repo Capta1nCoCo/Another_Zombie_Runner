7dfbf0105ab02bf1050fcd9bc06d3bc5930d86d8
Capta1nCoCo <femabu28@gmail.com>
2020-07-22 13:38:53 +0300

    198. Player Input To Select Weapon

--- Another_Zombie_Runner/Assets/Scripts/WeaponSwitcher.cs
+++ Another_Zombie_Runner/Assets/Scripts/WeaponSwitcher.cs
@@ -7,10 +7,64 @@ public class WeaponSwitcher : MonoBehaviour
 {
     [SerializeField] int currentWeapon = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
         SetWeaponActive();
+    }
+   
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWheel();
+
+        if (previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessScrollWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
     }
 
     private void SetWeaponActive()
@@ -31,9 +85,5 @@ public class WeaponSwitcher : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
