355f78055f72797715cc18abebfa7c14a41bbb26
Capta1nCoCo <femabu28@gmail.com>
2020-07-24 19:53:23 +0300

    213. Create A Flashlight (Completed)

--- Another_Zombie_Runner/Assets/Scripts/FlashlightSystem.cs
+++ Another_Zombie_Runner/Assets/Scripts/FlashlightSystem.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class FlashlightSystem : MonoBehaviour
 {
-    [SerializeField] float lightDecay = 1f;
-    [SerializeField] float angleDecay = 3f;
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
     [SerializeField] float minAngle = 40f;
 
     Light myLight;
@@ -15,29 +16,23 @@ public class FlashlightSystem : MonoBehaviour
     {
         myLight = GetComponent<Light>();
     }
-
-
+    
     void Update()
     {
-        while (myLight.intensity != 0)
-        {
-            var waitForSeconds = new WaitForSeconds(2f);
-            DecreaseLightAngle();
-            DecreaseLightIntensity();
-        }
-        
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
     }
 
     private void DecreaseLightAngle()
     {
         if (myLight.spotAngle > minAngle)
         {
-            myLight.spotAngle -= angleDecay;
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
         }        
     }
 
     private void DecreaseLightIntensity()
     {
-        myLight.intensity -= lightDecay;
+        myLight.intensity -= lightDecay * Time.deltaTime;
     }
 }
